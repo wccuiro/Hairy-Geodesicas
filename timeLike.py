914c2110e5d73c70cee16894c77a7addae50f731
Weyner Ccuiro <wey.090300@gmail.com>
2021-01-20 19:10:44 -0500

    alpha=-40 eta= 3.252719443 nu= 1.76 E=1 J=7e-7

--- timeLike.py
+++ timeLike.py
@@ -8,8 +8,8 @@ import os
 # creando todos los objetos
 miBH = bh.black_hole(-40, 3.252719443, 1.76)  # branch negativo
 miParticula = bh.particula_time_like(-40, 3.252719443, 1.76, 1, 7e-7)  # (bh,energia,J)
-miParticula1 = bh.particula_time_like(-40, 3.252719443, 1.76, 5, 7e-7)  # (bh,energia,J)
-miParticula2 = bh.particula_time_like(-40, 3.252719443, 1.76, 10, 7e-7)  # (bh,energia,J)
+# miParticula1 = bh.particula_time_like(-40, 3.252719443, 1.76, 5, 7e-7)  # (bh,energia,J)
+# miParticula2 = bh.particula_time_like(-40, 3.252719443, 1.76, 10, 7e-7)  # (bh,energia,J)
 
 #print(miBH.horizonte(),miBH.horizonte_hairy(),miBH.horizonte_hairy_x())
 print(miBH.masa())
@@ -18,8 +18,8 @@ print(miBH.masa())
 x_n, y_n = miParticula.cond_init(2)
 print(x_n, y_n)
 
-x_n1, y_n1 = miParticula1.cond_init(2)
-x_n2, y_n2 = miParticula2.cond_init(2)
+# x_n1, y_n1 = miParticula1.cond_init(2)
+# x_n2, y_n2 = miParticula2.cond_init(2)
 
 # geodesicas null
 theta_end = 2.5*ma.pi
@@ -29,22 +29,22 @@ theta = np.linspace(0, theta_end, s)
 r = bh.RK(x_n, y_n, h, s, miParticula)
 print(r)
 
-r1 = bh.RK(x_n1, y_n1, h, s, miParticula1)
-r2 = bh.RK(x_n2, y_n2, h, s, miParticula2)
+# r1 = bh.RK(x_n1, y_n1, h, s, miParticula1)
+# r2 = bh.RK(x_n2, y_n2, h, s, miParticula2)
 
 
 # para que sea en polares sin necesidad de transformar
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 ax.spines['polar'].set_visible(False)  # para quitarle el borde
 
-#ax.plot(theta, r, dashes=[4, 1])
+ax.plot(theta, r, dashes=[4, 1])
 
-l1, = ax.plot(theta, r, dashes=[4, 1])
-l2, = ax.plot(theta, r1, dashes=[2, 1])
-l3, = ax.plot(theta, r2, dashes=[1, 1])
+# l1, = ax.plot(theta, r, dashes=[4, 1])
+# l2, = ax.plot(theta, r1, dashes=[2, 1])
+# l3, = ax.plot(theta, r2, dashes=[1, 1])
 
-#ax.legend([f'E = {miParticula.energia}'], loc='upper right', shadow=True)
-ax.legend((l1, l2, l3), (f'E = {miParticula.energia}', f'E = {miParticula1.energia}', f'E = {miParticula2.energia}'), loc='upper right', shadow=True)
+ax.legend([f'E = {miParticula.energia}'], loc='upper right', shadow=True)
+# ax.legend((l1, l2, l3), (f'E = {miParticula.energia}', f'E = {miParticula1.energia}', f'E = {miParticula2.energia}'), loc='upper right', shadow=True)
 
 BH_schwarzschild = plt.Circle(
     (0, 0), miBH.horizonte(), transform=ax.transData._b, color='dimgrey')
@@ -54,8 +54,8 @@ BH_hairy = plt.Circle((0, 0), miBH.horizonte_hairy(),
 ax.add_artist(BH_schwarzschild)
 ax.add_artist(BH_hairy)
 
-ax.set_rmax(0.15)  # lim para time_like
-ax.set_rticks([0.08, 0.15])  # Less radial ticks
+ax.set_rmax(0.3)  # lim para time_like
+ax.set_rticks([0.08, 0.15, 0.3])  # Less radial ticks
 ax.set_xticks([])
 
 #ax.set_theta_zero_location("E", offset=-(theta[9289]/2+np.pi)/np.pi*180)
@@ -80,4 +80,4 @@ try:
 except FileExistsError:
     print("Listo")
 
-plt.savefig(f'./Figures/alpha={miParticula.alpha}_eta={miParticula.eta}_nu={miParticula.nu}/timeLike_orbits/E={miParticula.energia}-{miParticula1.energia}-{miParticula2.energia}_J={miParticula.J}.svg')
+plt.savefig(f'./Figures/alpha={miParticula.alpha}_eta={miParticula.eta}_nu={miParticula.nu}/timeLike_orbits/E={miParticula.energia}_J={miParticula.J}.svg')
