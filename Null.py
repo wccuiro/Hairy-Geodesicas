c2be94755054c911faf73f0bc7151309cc32d2d5
Weyner Ccuiro <wey.090300@gmail.com>
2021-02-11 16:28:18 -0500

    Null para el positive branch

--- Null.py
+++ Null.py
@@ -6,18 +6,20 @@ import math as ma
 import os
 
 # creando todos los objetos
-miBH = bh.black_hole(1, 12.527, 1.52)  # branch negativo
-miParticula = bh.particula_null(1, 12.527, 1.52, -0.025, 2.6e-6)  # (bh,energia,J)
+miBH = bh.black_hole(-40, 3.252719443, 1.76)  # branch negativo
+miParticula = bh.particula_null(-40, 3.252719443, 1.76, 0.03)  # (bh,b)
 
-# condiciones iniciales de los objetos para geodesicas tipo time like
-x_n, y_n = miParticula.cond_init(0.4)
+# condiciones iniciales de los objetos para geodesicas tipo null
+x_n, y_n, theta_n = miParticula.cond_init(1)
+
+print(theta_n)
 
 # geodesicas null
-theta_end = 8.5*ma.pi
+theta_end = 1.7*ma.pi+theta_n
 s = 10000
-h = (theta_end-0)/s
-theta = np.linspace(0, theta_end, s)
-r = bh.RK(x_n, -y_n, h, s, miParticula)
+h = (theta_end-theta_n)/s
+theta = np.linspace(theta_n, theta_end, s)
+r = bh.RK(x_n, y_n, h, s, miParticula)
 
 # para que sea en polares sin necesidad de transformar
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
@@ -26,7 +28,7 @@ ax.spines['polar'].set_visible(False)  # para quitarle el borde
 ax.plot(theta, r, dashes=[4, 1])
 
 #ax.legend((l, l1, l2), (f'E = {miParticula.energia}', f'E = {miParticula1.energia}',f'E = {miParticula2.energia}'), loc='upper right', shadow=True)
-ax.legend([f'E = {miParticula.energia}'], loc='upper right', shadow=True)
+ax.legend([f'b = {miParticula.b}'], loc='upper right', shadow=True)
 
 
 #ax.set_title("Desviación de Einstein", va='bottom')
@@ -40,8 +42,8 @@ BH_hairy = plt.Circle((0, 0), miBH.horizonte_hairy(),
 ax.add_artist(BH_schwarzschild)
 ax.add_artist(BH_hairy)
 
-ax.set_rmax(3)  # lim para time_like
-ax.set_rticks([0.08, 1, 2, 3])  # Less radial ticks
+ax.set_rmax(0.15)  # lim para time_like
+ax.set_rticks([0.08, 0.15])  # Less radial ticks
 ax.set_xticks([])
 
 
@@ -51,7 +53,13 @@ ax.grid(True)
 
 try:
     os.mkdir('./Figures')
+except:
+    pass
+try:
     os.mkdir(f'./Figures/alpha={miParticula.alpha}_eta={miParticula.eta}_nu={miParticula.nu}')
+except:
+    pass
+try:
     os.mkdir(f'./Figures/alpha={miParticula.alpha}_eta={miParticula.eta}_nu={miParticula.nu}/Null_orbits')
 except FileExistsError:
     print("Listo")
